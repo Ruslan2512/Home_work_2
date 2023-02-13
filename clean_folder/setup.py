fb802cffeb60087ca96d72499e66c99c36230158
Ruslan2512 <ruslansirenko25@gmail.com>
2023-02-13 23:39:01 +0200

    Initial commit

---
+++ clean_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='clean_folder',
+    version='1',
+    description='Sort files in directiry',
+    url='https://github.com/Ruslan2512/Home_work_2',
+    author='Ruslan Sirenko',
+    author_email='ruslansirenko25@gmail.com',
+    entry_points={'console_scripts': ['clean-folder = clean_folder.sort_files:sort_files']}
+)
